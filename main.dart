74551ba3fe6a1f6c883c096d2456b721f5b8efdf
devJuanMiarelli <136279190+devJuanMiarelli@users.noreply.github.com>
2024-04-13 17:42:58 -0300

    Update main

--- main.dart
+++ main.dart
@@ -1,17 +1,17 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'App para consumir API',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      title: 'Consumo de API',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: HomePage(),
     );
   }
@@ -19,115 +19,50 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatefulWidget {
   @override
-  _HomePageState createState() {
-      return _HomePageState();
-  }  
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Lista de produtos'),
-        ),
-        body: ListView.separated(
-          separatorBuilder: (BuildContext context, int index) => Divider(),
-          itemCount: products.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Text(products[index]['description'])
-            );
-          }
-        )
-      );
-    }
-  
   List<dynamic> products = [];
-    
+
   @override
   void initState() {
     super.initState();
     fetchPosts();
   }
-  
+
   Future<void> fetchPosts() async {
-    final response = await http.get(Uri.parse('https://app-uniara-eb91fc9ec7bf.herokuapp.com/list'));
-    
-    if(response.statusCode == 200) {
-      setState() {
+    final response = await http.get(
+        Uri.parse('https://app-uniara-eb91fc9ec7bf.herokuapp.com/list'),
+        headers: {'Content-Type': 'application/json; charset=utf8'});
+
+    if (response.statusCode == 200) {
+      setState(() {
         String data = response.body;
+
+        print("antes: $data");
+        data = latin1.decode(latin1.encode(data));
+        print("depois: $data");
         products = json.decode(data);
-      };
+      });
     } else {
-      throw Exception('404 Não encontrado ;(');
+      throw Exception('Failed to load posts');
     }
-}
-
-}import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'App para consumir API',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomePage(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Produtos'),
+      ),
+      body: ListView.separated(
+        itemCount: products.length,
+        separatorBuilder: (BuildContext context, int index) => Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(title: Text(products[index]['description']));
+        },
+      ),
     );
   }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() {
-      return _HomePageState();
-  }  
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Lista de produtos'),
-        ),
-        body: ListView.separated(
-          separatorBuilder: (BuildContext context, int index) => Divider(),
-          itemCount: products.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Text(products[index]['description'])
-            );
-          }
-        )
-      );
-    }
-  
-  List<dynamic> products = [];
-    
-  @override
-  void initState() {
-    super.initState();
-    fetchPosts();
-  }
-  
-  Future<void> fetchPosts() async {
-    final response = await http.get(Uri.parse('https://app-uniara-eb91fc9ec7bf.herokuapp.com/list'));
-    
-    if(response.statusCode == 200) {
-      setState() {
-        String data = response.body;
-        products = json.decode(data);
-      };
-    } else {
-      throw Exception('404 Não encontrado ;(');
-    }
-}
-
 }
